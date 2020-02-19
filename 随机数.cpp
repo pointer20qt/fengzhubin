86fb458386283b278d892f5068d8eb5573d8f497
91BinVon <359134733@qq.com>
2020-02-19 21:55:32 +0800

    commit message

---
+++ 随机数.cpp
@@ -0,0 +1,17 @@
+#include <time.h> 
+#include <iostream>
+#include<math.h>
+using namespace std;
+
+
+int mian()
+{
+	srand(time(0));
+	int p = rand % 100;
+	
+	cout << p << endl;
+	
+	
+	return 0;
+	
+}
